9dbd1098a7dfbf781b153537a648600ca5b711dd
Chris A. Duncan <caduncan3@gmail.com>
2015-02-09 18:52:27 -0800

    got rid of l

--- profanity check.py
+++ profanity check.py
@@ -1,6 +1,6 @@
 import urllib
 
-def read_text(): 
+def read_text():
 	quotes= open("C:\detect_profanity\movie_quotes.txt")
 	contents_of_file= quotes.read()
 	print(contents_of_file)
@@ -8,10 +8,10 @@ def read_text():
 	check_profnaity(contents_of_file)
 
 
-def check_profanitiy(text_tocheck): 
+def check_profanitiy(text_tocheck):
 	 connection = urllib.urlopen("http://www.wdyl.com/profanity?q=shot")
 	 print(output)
 	 connection.close()
-l
 
-read_text() 
+
+read_text()
